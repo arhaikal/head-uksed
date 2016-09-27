a5090e44c32ff5bea7a5f2af5a59eeb6db68df1f
Andre Haikal <andrehaykal@gmail.com>
2016-09-28 02:01:24 +0300

    Fixed footer, styled show page, and active classes for navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+   def current_class?(test_path)
+    return 'active1' if request.path == test_path
+    ''
+   end 
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ActiveRecord::Base
   extend FriendlyId
   friendly_id :title, use: [:slugged, :finders]
 
-  has_attached_file :image, styles: { medium: "150x350#" }
+  has_attached_file :image, styles: { medium: "150x350#", large: "250x500#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
