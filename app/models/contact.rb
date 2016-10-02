3f6e0b31015047e4a87570c5aa1c563e8c5a6a32
Andre Haikal <andrehaykal@gmail.com>
2016-10-03 01:13:19 +0300

    from test 3

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < MailForm::Base
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :nickname,  :captcha  => true
-  attributes :image
+  attribute :file,      :attachment => true
 
   def headers
     {
